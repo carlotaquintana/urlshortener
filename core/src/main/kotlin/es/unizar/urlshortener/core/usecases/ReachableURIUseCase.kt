5c77b869b4ce017a48c4d5b67b97d1f4691de0e8
Leonor Murphy <76400289+leonormurphy@users.noreply.github.com>
2023-11-12 14:33:46 +0100

    3 peticiones http GET espaciadas 1 segundo

--- core/src/main/kotlin/es/unizar/urlshortener/core/usecases/ReachableURIUseCase.kt
+++ core/src/main/kotlin/es/unizar/urlshortener/core/usecases/ReachableURIUseCase.kt
@@ -4,32 +4,51 @@ import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
+import java.time.Duration
 
 /*
 * We want to verify that a URI is reachable.
+*
+* *[reachable]* checks if a URI is reachable or not.
+*
 */
 
 interface ReachableURIUseCase {
     fun reachable(uri: String): Boolean
+
 }
 
+/**
+ *
+ */
 class ReachableURIUseCaseImpl : ReachableURIUseCase {
+    private val MAX_ATTEMPTS = 3
+    private val ATTEMPT_DELAY_SECONDS = 1L
     override fun reachable(uri: String): Boolean {
-        // To verify if a URI is reachable we need to send a request to the URI and check the response.
-        // We create a new HTTP client.
-        val client = HttpClient.newHttpClient()
+        var attempts = 0
+        while (attempts < MAX_ATTEMPTS) {
+            val client = HttpClient.newHttpClient()
+            val request = HttpRequest.newBuilder()
+                .uri(URI.create(uri))
+                .timeout(Duration.ofSeconds(ATTEMPT_DELAY_SECONDS))
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build()
 
-        // We create a new HTTP request.
-        val request = HttpRequest.newBuilder()
-            .uri(URI.create(uri))
-            .method("HEAD", HttpRequest.BodyPublishers.noBody())
-            .build()
+            val response = try {
+                client.send(request, HttpResponse.BodyHandlers.discarding())
+            } catch (e: Exception) {
+                // Manejar cualquier excepción que pueda ocurrir durante la solicitud
+                null
+            }
 
-        // We send the request and check the response.
-        val response = client.send(request, HttpResponse.BodyHandlers.discarding())
+            if (response != null && response.statusCode() == 200) {
+                return true
+            }
 
-        // If the response is 200, the URI is reachable. If not, it isn't reachable.
-        return response.statusCode() == 200
+            attempts++
+            Thread.sleep(ATTEMPT_DELAY_SECONDS * 1000) // Espaciar las solicitudes en 1 segundo
+        }
+        return false
 
     }
 }
