c0c615d7df51cb832a2ced83d635a023b8d4acb1
Leonor Murphy <76400289+leonormurphy@users.noreply.github.com>
2023-12-05 15:06:45 +0100

    TODO en caso de alcanzable/no alcanzable

--- core/src/main/kotlin/es/unizar/urlshortener/core/queues/ReachableQueue.kt
+++ core/src/main/kotlin/es/unizar/urlshortener/core/queues/ReachableQueue.kt
@@ -12,23 +12,36 @@ import java.util.concurrent.ConcurrentLinkedQueue
 class ReachableQueue {
     private val queue: Queue<Any> = ConcurrentLinkedQueue()
     private val reachableUseCase: ReachableURIUseCase = ReachableURIUseCaseImpl()
+
+    /**
+     * Se añade un elemento a la cola.
+     */
     fun addToQueue(item: Any?) {
         queue.add(item)
     }
+
+    /**
+     * Se obtiene un elemento de la cola.
+     */
     private fun getFromQueue(): Any {
         return queue.poll()
     }
 
-    // Se verifica la alcanzabilidad de forma asíncrona. Se crea un nuevo hilo de ejecución en el que
-    // se va a estar comprobando la cola de URLs a verificar.
-
+    /** Se verifica la alcanzabilidad de forma asíncrona. Se crea un nuevo hilo de ejecución en el que
+     * se va a estar comprobando la cola de URLs a verificar.
+     */
     fun verificarAlcanzabilidad() {
         val thread = Thread(Runnable {
             while (true) {
                 val item = getFromQueue()
                 if (item != null) {
                     // Verificar alcanzabilidad de la URI
-                    reachableUseCase.reachable(item.toString())
+                    if(reachableUseCase.reachable(item.toString())){
+                        TODO("Si es alcanzable...")
+                    }
+                    else{
+                        TODO("Si no es alcanzable, ¿enviar 201 a POST?")
+                    }
                 }
             }
         })
